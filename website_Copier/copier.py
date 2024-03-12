6237bcc7354b6debeb2c3db7c8e7f6dad2aaeb00
Klayton Pagel <pagelk@lopers.unk.edu>
2024-03-12 11:59:56 -0500

    working on copier

--- website_Copier/copier.py
+++ website_Copier/copier.py
@@ -1,7 +1,24 @@
+import os
 import shutil
 
-destination_path = 'C:/Users/pagel/OneDrive/Documents/Saved_Files/GitHub/klaytonpagel.github.io'
+destination_path = '../../klaytonpagel.github.io'
+source_folder = os.listdir("../../html-practice")
+
+print(source_folder)
 
 
+def make_list():
+    excluded_files = [".git", ".idea", "role-playing-game", "calorie counter", "canvas_testing"]
+    for file in source_folder:
+        if file in excluded_files:
+            print(file)
 
-shutil.copy2(source_path, destination_path)
+
+def copy_file(source_path):
+    shutil.copy2(source_path, destination_path)
+
+
+make_list()
+
+
+# shutil.copy2(source_path, destination_path)
