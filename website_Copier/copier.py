84c6fcdd540aa22f86fac69754d10a6328d1fe56
Klayton Pagel <pagelk@lopers.unk.edu>
2024-03-12 12:09:22 -0500

    Update copier.py

--- website_Copier/copier.py
+++ website_Copier/copier.py
@@ -1,24 +1,34 @@
 import os
 import shutil
 
-destination_path = '../../klaytonpagel.github.io'
-source_folder = os.listdir("../../html-practice")
 
-print(source_folder)
+class Copier:
 
+    # Set up the destination and get the files in the source folder_____________________________________________________
+    def __init__(self):
+        self.destination_path = '../../klaytonpagel.github.io'
+        self.source_folder = os.listdir("../../html-practice")
+        self.source_path_base = "../../html-practice/"
+        self.source_list = []
 
-def make_list():
-    excluded_files = [".git", ".idea", "role-playing-game", "calorie counter", "canvas_testing"]
-    for file in source_folder:
-        if file in excluded_files:
-            print(file)
+        self.make_list()
+        self.run()
 
+    # Construct a list of files to copy excluding ones I don't want to copy_____________________________________________
+    def make_list(self):
+        excluded_files = [".git", ".idea", "role-playing-game", "calorie counter", "canvas_testing"]
+        for file in self.source_folder:
+            if file in excluded_files:
+                self.source_list.append(file)
 
-def copy_file(source_path):
-    shutil.copy2(source_path, destination_path)
+    # Copy the source file to the destination folder____________________________________________________________________
+    def copy_file(self, source_path):
+        shutil.copy2(source_path, self.destination_path)
 
+    # For each file in the source list turn it into the source path then copy it________________________________________
+    def run(self):
+        for file in self.source_list:
+            source_path = self.source_path_base + file
+            self.copy_file(source_path)
 
-make_list()
-
-
-# shutil.copy2(source_path, destination_path)
+Copier()
