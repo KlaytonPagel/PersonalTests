05bee0fdeeac5a62fe2b3d10bbafa6d11bdee40c
Klayton Pagel <pagelk@lopers.unk.edu>
2024-03-14 16:01:19 -0500

    Updated Copier

--- website_Copier/copier.py
+++ website_Copier/copier.py
@@ -6,7 +6,8 @@ class Copier:
 
     # Set up the destination and get the files in the source folder_____________________________________________________
     def __init__(self):
-        self.destination_path = '../../klaytonpagel.github.io'
+        print("starting")
+        self.destination_path_base = '../../klaytonpagel.github.io/'
         self.source_folder = os.listdir("../../html-practice")
         self.source_path_base = "../../html-practice/"
         self.source_list = []
@@ -18,17 +19,25 @@ class Copier:
     def make_list(self):
         excluded_files = [".git", ".idea", "role-playing-game", "calorie counter", "canvas_testing"]
         for file in self.source_folder:
-            if file in excluded_files:
+            if file not in excluded_files:
                 self.source_list.append(file)
 
     # Copy the source file to the destination folder____________________________________________________________________
-    def copy_file(self, source_path):
-        shutil.copy2(source_path, self.destination_path)
+    def copy_file(self, source_path, destination_path):
+        if os.path.isdir(source_path):
+            os.rmdir(destination_path)
+            shutil.copytree(source_path, destination_path)
+        else:
+            shutil.copy2(source_path, self.destination_path_base)
 
     # For each file in the source list turn it into the source path then copy it________________________________________
     def run(self):
         for file in self.source_list:
             source_path = self.source_path_base + file
-            self.copy_file(source_path)
+            destination_path = self.destination_path_base + file
+            print("copying", source_path, "to", destination_path)
+            self.copy_file(source_path, destination_path)
+        print("done")
+
 
 Copier()
